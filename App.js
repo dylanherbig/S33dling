7a52c40d2db725af48a44888b90e1277357e4f3d
dylanherbig <dylanherbig@gmail.com>
2022-09-17 17:02:31 -0400

    added navigation bar, no reroute functionality yet

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import PlantCamera from './components/Camera';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <PlantCamera />
       <Text>Bye Amrit</Text>
+      <NavBar />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/NavBar.js
@@ -0,0 +1,59 @@
+// styling
+import {
+    StyleSheet,
+    View,
+    Pressable,
+} from 'react-native';
+
+// icon
+import Icon from 'react-native-ico-material-design';
+
+const NavBar = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.bar}>
+                <Pressable onPress={() => console.log("Profile")} style={styles.iconBehave}
+                android_ripple={{borderless: true, radius: 50}}>
+                    <Icon name="user-shape" height={30} width={30} color="#fff" backgroundColor="#c1f0b7"/>
+                </Pressable>
+
+                <Pressable onPress={() => console.log("Home")} style={styles.iconBehave}
+                android_ripple={{borderless: true, radius: 50}}>
+                    <Icon name="home-button" height={30} width={30} color="#fff"/>
+                </Pressable>
+
+                <Pressable onPress={() => console.log("Camera")} style={styles.iconBehave}
+                android_ripple={{borderless: true, radius: 50}}>
+                    <Icon name="camera" height={30} width={30} color="#fff"/>
+                </Pressable>
+
+                <Pressable onPress={() => console.log("Plants")} style={styles.iconBehave}
+                android_ripple={{borderless: true, radius: 50}}>
+                    <Icon name="view-list-button" height={30} width={30} color="#fff"/>
+                </Pressable>
+
+            </View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        alignItems: 'center',
+        bottom: 20,
+    },
+    bar: {
+        flexDirection: 'row',
+        backgroundColor: '#2a7e19',
+        width: '90%',
+        justifyContent: 'space-evenly',
+        borderRadius: 40,
+    },
+    iconBehave: {
+        padding: 16,
+    }
+});
+
+export default NavBar;
