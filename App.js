e2a372a11ed8c2c1cb3d9a9b07eb6afbb05dabdf
jmheilemann <jmheilemann@gmail.com>
2022-09-18 02:22:32 -0400

    tplease

--- App.js
+++ App.js
@@ -2,16 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './routes/homeStack';
 
-import { useState, useEffect } from "react";
-import {
-  ref,
-  uploadBytes,
-  getDownloadURL,
-  listAll,
-  list,
-} from "firebase/storage";
-import { storage } from "./firebase";
-import { v4 } from "uuid";
+
 
 
 // import  Pixelify  from "react-pixelify";
@@ -20,30 +11,7 @@ import { v4 } from "uuid";
 
 
 export default function App() {
-  const [imageUpload, setImageUpload] = useState(null);
-  const [imageUrls, setImageUrls] = useState([]);
-
-  const imagesListRef = ref(storage, "images/"); // images from firebase
-  const uploadFile = () => {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageUrls((prev) => [...prev, url]);
-      });
-    });
-  };
-
-  // gets images from firebase bucket
-  // useEffect(() => {
-  //   listAll(imagesListRef).then((response) => {
-  //     response.items.forEach((item) => {
-  //       getDownloadURL(item).then((url) => { //download url for each item
-  //         setImageUrls((prev) => [...prev, url]);
-  //       });
-  //     });
-  //   });
-  // }, []);
+  
   
     return (
       <View>
--- pages/home.js
+++ pages/home.js
@@ -2,16 +2,63 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import NavBar from '../components/NavBar';
 
+import { useState, useEffect } from "react";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
+import { storage } from "../firebase";
+import { v4 } from "uuid";
+
 export default function Home({ navigation }) {
 
   const pressHandler = () => {
     navigation.push('Plant');
   }
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const imagesListRef = ref(storage, "images/"); // images from firebase
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  //gets images from firebase bucket
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => { //download url for each item
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text>home page</Text>
       <Button title='plant Bob' onPress={pressHandler} />
+      
+      {/* <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      /> */}
+      <Button title='Upload' onPress={uploadFile} />
+      {imageUrls.map((url) => {
+        return <img src={url} />;
+      })}
+
       <NavBar />
       <StatusBar style="auto" />
     </View>
